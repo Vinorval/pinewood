731e7ce477818dd3cee0bc008aefc3e5f59601cd
Vinor <vinorval@yandex.ru>
2023-06-09 10:12:00 +0300

    add page navigation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <header className="App-header"></header>
+      <Routes>
+        <Route path="/" />
+        <Route path="/projects" />
+        <Route path="/service" />
+        <Route path="/company" />
+        <Route path="/contact" />
+        <Route path="/project" />
+        <Route path="/price" />
+        <Route path="*" />
+      </Routes>
     </div>
   );
 }
